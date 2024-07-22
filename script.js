72a566f09190dab5599a907f415c7f91554bbf07
Najeeba <najeebaqarqin@gmail.com>
2024-07-22 23:48:08 +0430

    Added code for highlite the navbar links when window scrolled

--- script.js
+++ script.js
@@ -21,3 +21,30 @@ closeList.addEventListener('click', () => {
   closeList.classList.toggle('hidden');
   humburegerMenu.classList.toggle('hidden');
 });
+
+const sections = document.querySelectorAll('section');
+const menuLinks = document.querySelectorAll('nav ul li a');
+
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function handleScroll() {
+  sections.forEach(section => {
+    if (isInViewport(section)) {
+      const sectionId = section.id;
+      menuLinks.forEach(link => {
+        link.classList.remove('active');
+      });
+      document.querySelector(`nav > ul > li > a[href="#${sectionId}"]`).classList.add('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', handleScroll);
