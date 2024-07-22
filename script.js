a0b657c29a80c5886878d868576f546ba754e54e
Najeeba <najeebaqarqin@gmail.com>
2024-07-22 23:50:55 +0430

    Added dynamic contents to home and about sections

--- script.js
+++ script.js
@@ -48,3 +48,51 @@ function handleScroll() {
 }
 
 window.addEventListener('scroll', handleScroll);
+
+
+const home = document.getElementById('home');
+
+home.innerHTML = `
+<img class="images" src="./images/home.jpg" alt="Profile" />
+<h3>Hello, <span class="span">there!</span></h3>
+<b><span class='span'>I'm</span> Najeeba Qarqin</b>
+<strong>FrontEnd <span class='span'>Web Developer</span></strong>
+<p class='para'>Skilled in HTML, CSS and javaScript languages and Git, GitHub and VS code tools, interested to design and develope great websites.</p>
+<div>
+<a class='button' href='#about'>Learn More</a>
+<a class='button' href='#contact'>Contact Me</a>
+</div>
+`
+
+const about = document.getElementById('about-content');
+
+const skills = {
+  languages: ['HTML', 'CSS', 'javaScript'],
+  tools: ['Git', 'GitHUb', 'VS Code'],
+  certification: [
+    { name: 'Web Design', link: 'www.com', image: './images/webDesign.png' },
+    { name: 'JavaScript', link: 'www.com', image: './images/JS.png' },
+  ],
+};
+
+about.innerHTML = `
+  <img class="images" src="images/about.jpg" alt="">
+  <p class='para' id='about-p'>I'm <b class='span'>Najeeba Qarqin</b> a Web Developement student at Kick-Start i learned responsive web design and javaScript programing language and attained the free code camp certifications of Responsive Web Design and JavaScript Algorthms And Data Structure programs.
+  Actively i'm increasing my web developing skills to be a profissional web developer and apply my acquired skills and knowlage to real-world projects.</p>
+  <p class='para'>Email: <a href='https://najeebaqarqin@gmail.com' class='span'>najeebaqarqin@gmail.com</a></p>
+  <p class='para'><b class='span'>Languages</b>: ${skills.languages.join(', ')}</p>
+  <p class='para'><b class='span'>Tools</b>: ${skills.tools.join(', ')}</p>
+  <p>My <b class='span'>Certifications</b>: </p>
+  <div class="certificates-container flex">
+  ${skills.certification.map((certificate) =>
+  `
+  <div>
+    <a href='${certificate.link}' target='_blank'>
+      <img src='${certificate.image}' alt='${certificate.name}' class="certificates">
+      </a>
+      <p>${certificate.name}</p>
+      </div>
+      `
+).join('')}
+      </div>
+    `;
